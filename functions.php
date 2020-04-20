73d00444c472fb34b515a3f9cb6b8644136e2b79
Mischa Sigtermans <mischa.sigtermans@icloud.com>
2020-04-20 17:01:11 +0200

    Remove default thumbnail sizes

--- functions.php
+++ functions.php
@@ -1,4 +1,18 @@
 <?php
 
 // Enable featured image block
-add_theme_support( 'post-thumbnails' );
+add_theme_support('post-thumbnails');
+
+// Remove default thumbnail sizes
+function nothing_remove_default_images($sizes)
+{
+    unset($sizes['thumbnail']);
+    unset($sizes['medium']);
+    unset($sizes['medium_large']);
+    unset($sizes['large']);
+    unset($sizes['1536x1536']);
+    unset($sizes['2048x2048']);
+
+    return $sizes;
+}
+add_filter('intermediate_image_sizes_advanced', 'nothing_remove_default_images');
